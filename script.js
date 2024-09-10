9944cc369906b277c0a81eed65693222bc1f5d08
Vaishali7599 <140495282+Vaishali7599@users.noreply.github.com>
2024-09-10 23:18:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+	let inp = document.querySelectorAll("input")
+
+	let a = inp[0].value;
+	let b = inp[1].value;
+	let ans = a + " " + b
+	alert(ans)
+	
 
 }
